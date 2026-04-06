65a00717c844bbc22b67beef51b196cbe883844d
ryuma-1 <pznt47vx@s.okayama-u.ac.jp>
2026-04-06 18:31:12 +0900

    Make create-task

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::io;
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
@@ -14,6 +15,8 @@ enum Commands {
     GetTask {
         path: i32,
     },
+    CreateTask {
+    },
 }
 
 trait Executable {
@@ -25,6 +28,7 @@ impl Executable for Commands {
         match self {
             Commands::GetAllTasks {} => {
                 let token = env::var("RASK_API_TOKEN")?;
+                println!("Token: {}", token);
                 let res = reqwest::blocking::get(&format!("http://localhost:3000/tasks.json?api_token={}", token))?;
                 println!("Status: {}", res.status());
                 let body = res.text()?;
@@ -42,16 +46,56 @@ impl Executable for Commands {
                 println!("{}", body);
                 Ok(())
             }
+
+            Commands::CreateTask {} => {
+                let token = env::var("RASK_API_TOKEN")?;
+
+                let assigner_id = input("assigner_id:");
+                let due_at = input("due_at:");
+                let task_state_id = input("task_state_id:");
+                let content = input("content:");
+                let description = input("description:");
+
+                let client = reqwest::blocking::Client::new();
+
+                let res = client.post(
+                    &format!("http://localhost:3000/tasks.json?api_token={}", token)
+                )
+                .json(&serde_json::json!({
+                    "task": {
+                        "assigner_id": assigner_id,
+                        "due_at": due_at,
+                        "task_state_id": task_state_id,
+                        "content": content,
+                        "description": description,
+                    }
+                }))
+                .send()?;
+
+                println!("Status: {}", res.status());
+                let body = res.text()?;
+                println!("{}", body);
+                Ok(())
+            }
         }
     }
 }
 
+fn input(string: &str) -> String {
+    let mut input = String::new();
+    println!("{}", string);
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input.trim().to_string()
+}
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv::dotenv().ok();
+
     let cli = Cli::parse();
-
     cli.command.execute()?;
-
     Ok(())
 }
 
