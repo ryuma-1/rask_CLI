7d06f1b0ceed6cf52cbba7899afda9b40f5c6074
ryuma-1 <pznt47vx@s.okayama-u.ac.jp>
2026-05-26 12:52:21 +0900

    Add api client in main.rs

--- src/rask_command.rs
+++ src/rask_command.rs
@@ -3,7 +3,6 @@ use chrono::{DateTime, NaiveDate, Utc};
 use clap::builder::Str;
 use clap::Subcommand;
 use regex::Regex;
-use std::env;
 use std::f32::consts::E;
 
 use crate::doc;
@@ -20,19 +19,9 @@ pub enum RaskCommand {
     GetTask {
         id: i32,
     },
-    CreateTask {},
     GetAllDocs {},
     GetDoc {
         id: i32,
-    },
-    CreateDoc {},
-    SearchTodayDoc {
-        #[arg(value_enum)]
-        m_type: MinuteType,
-    },
-    SearchDocByKeyword {
-        #[arg(num_args = 1..)]
-        keywords: Vec<String>,
     },
     SearchDoc {
         #[arg(long)]
@@ -77,152 +66,34 @@ pub enum RaskCommand {
 }
 
 pub trait Executable {
-    fn execute(self) -> anyhow::Result<()>;
+    fn execute(self, rask_api: RaskApiClient) -> Result<()>;
 }
 
 impl Executable for RaskCommand {
-    fn execute(self) -> anyhow::Result<()> {
-        // 1. クライアントとトークンを共通で準備
-        let token = env::var("RASK_API_TOKEN")
-            .map_err(|_| anyhow::anyhow!("環境変数 RASK_API_TOKEN が設定されていません。"))?;
-        let url = env::var("API_BASE_URL")
-            .map_err(|_| anyhow::anyhow!("環境変数 API_BASE_URL が設定されていません。"))?;
-
-        let api = RaskApiClient::new(token, url);
-
-        // 2. match の中身を整理
+    fn execute(self, rask_api: RaskApiClient) -> Result<()> {
         match self {
             RaskCommand::GetAllTasks {} => {
-                let res = api.get_all_tasks()?;
+                let res = rask_api.get_all_tasks()?;
                 print_response(res)?;
                 Ok(())
             }
 
             RaskCommand::GetTask { id } => {
-                let res = api.get_task(id)?;
-                print_response(res)?;
-                Ok(())
-            }
-
-            RaskCommand::CreateTask {} => {
-                let assigner_id = InputUtils::execute::<AssignerId>("assigner_id:");
-                let content = InputUtils::execute::<Content>("content:");
-                let due_at = InputUtils::execute::<DueAt>("due_at:");
-                let description = InputUtils::execute::<Description>("description:");
-                let project_id = InputUtils::execute::<ProjectId>("project_id:");
-                let task_state_id = InputUtils::execute::<TaskStateId>("task_state_id:");
-
-                let task = Task::new(
-                    assigner_id,
-                    content,
-                    due_at,
-                    description,
-                    project_id,
-                    task_state_id,
-                );
-
-                let task_req = TaskReq::new(task);
-                let json = serde_json::to_value(&task_req)?;
-
-                let res = api.create_task(json)?;
-
+                let res = rask_api.get_task(id)?;
                 print_response(res)?;
                 Ok(())
             }
 
             RaskCommand::GetAllDocs {} => {
-                let res = api.get_all_docs()?;
+                let res = rask_api.get_all_docs()?;
                 print_response(res)?;
                 // print_response(res)?;
                 Ok(())
             }
 
             RaskCommand::GetDoc { id } => {
-                let res = api.get_doc(id)?;
+                let res = rask_api.get_doc(id)?;
                 print_response(res)?;
-                Ok(())
-            }
-
-            RaskCommand::CreateDoc {} => {
-                let content = InputUtils::execute::<Content>("content:");
-                let description = InputUtils::execute::<Description>("description:");
-                let project_id = InputUtils::execute::<ProjectId>("project_id:");
-                let start_at = InputUtils::execute::<StartAt>("start_at:");
-                let end_at = InputUtils::execute::<EndAt>("end_at:");
-                let location = InputUtils::execute::<Location>("location:");
-
-                let doc_req =
-                    DocReq::new(content, description, project_id, start_at, end_at, location);
-
-                let json = serde_json::to_value(&doc_req)?;
-
-                let res = api.create_doc(json)?;
-
-                print_response(res)?;
-                Ok(())
-            }
-
-            RaskCommand::SearchTodayDoc { m_type } => {
-                // ここはAPI側で日付検索のエンドポイントがある前提で実装
-                // let date: NaiveDate = ds.parse().unwrap();
-                let res = api.get_all_docs()?;
-                let doc_res: Vec<DocRes> = serde_json::from_str(&res.text()?)?;
-
-                // タイトルにGNやNewが含まれているドキュメントをフィルタリング
-                let filtered_type_docs: Vec<DocRes> = doc_res
-                    .into_iter()
-                    .filter(|doc| doc.content().to_type() == m_type)
-                    .collect();
-
-                // 戻り値を Result<Vec<Minute>> にする
-                let minutes: Result<Vec<Minute>> = filtered_type_docs
-                    .into_iter()
-                    .map(|doc| {
-                        let content = doc.content().value();
-                        let num = MinuteNum::new(&content)?; // ここで ? を使ってエラーを上に投げる
-
-                        Ok(Minute::new(m_type, num, doc.url().clone()))
-                    })
-                    .collect(); // Result の Vec を collect すると、自動的に Result<Vec> になる
-
-                // 1. まず Vec を取り出して、変数にしっかり固定する
-                let minutes_vec = minutes?;
-
-                // 2. 変数に固定された Vec に対して iter() を呼ぶ
-                // これで Vec はこのスコープが終わるまで生存します
-                let max_minute = minutes_vec.iter().max_by_key(|minute| minute.num().value());
-
-                match max_minute {
-                    Some(minute) => {
-                        println!("{}", minute.url().trim_json());
-                    }
-                    None => {
-                        eprintln!("Error: max_minute is None"); // Ruby側で 2>&1 すれば見える
-                    }
-                }
-                Ok(())
-            }
-            RaskCommand::SearchDocByKeyword { keywords } => {
-                let res = api.get_all_docs()?;
-
-                let doc_res: Vec<DocRes> = serde_json::from_str(&res.text()?)?;
-
-                // すべてのキーワードが含まれているドキュメントをフィルタリング (AND検索)
-                let filtered_docs: Vec<DocRes> = doc_res
-                    .into_iter()
-                    .filter(|doc| {
-                        keywords
-                            .iter()
-                            .all(|keyword| doc.content().value().contains(keyword))
-                    })
-                    .collect();
-
-                // フィルタリングしたドキュメントのJSONを表示
-                for doc in filtered_docs {
-                    let doc_json = serde_json::to_string(&doc)?;
-                    println!("{}", doc_json);
-                }
-
                 Ok(())
             }
 
@@ -241,7 +112,7 @@ impl Executable for RaskCommand {
                 term_day,
                 is_visual,
             } => {
-                let res = api.get_all_docs()?;
+                let res = rask_api.get_all_docs()?;
 
                 let doc_res: Vec<DocRes> = serde_json::from_str(&res.text()?)?;
 
